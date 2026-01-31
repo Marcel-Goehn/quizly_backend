353b216eb9d08416225dd454e52c64c0a4f066c8
Marcel Göhn <marcelgoehn@googlemail.com>
2026-01-31 20:20:22 +0100

    feat: Added login

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.models import User
+
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from .serializers import RegistrationSerializer
 
 
@@ -15,8 +23,48 @@ class RegistrationView(APIView):
             return Response(
                 {"detail": "User created successfully!"},
                 status=status.HTTP_201_CREATED
-                )
+            )
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
+
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        refresh = response.data.get("refresh")
+        access = response.data.get("access")
+
+        response.set_cookie(
+            key="access_token",
+            value=access,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh,
+            httponly=True,
+            secure=True,
+            samesite="Lax"
+        )
+
+        try:
+            user = User.objects.get(username=request.data["username"])
+        except User.DoesNotExist:
+            return Response("User not found.")
+        
+        response.data = {
+            "detail": "Login successfully!",
+            "user": {
+                "id": user.pk,
+                "username": user.username,
+                "email": user.email
+            }
+        }
+        return response
